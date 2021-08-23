b79cb245f60aadd746c153ba432c805ecce7eee9
César Córdoba <cordobacesar29@gmail.com>
2021-08-23 17:26:19 -0300

    varios

--- component/Carousel.js
+++ component/Carousel.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import  {StatusBar,View, Image, Text, StyleSheet, Dimensions,FlatList, Animated} from 'react-native';
+import {StatusBar,View, Image, Text, StyleSheet, Dimensions, Animated, TouchableHighlight} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+import CarouselItem from './CarouselItem';
+
 import { dummyData, bgs } from '../data/Data';
+import { styles } from './style';
 
 const {width, height} = Dimensions.get('window');
 const data = dummyData;
@@ -9,15 +14,32 @@ const Indicator = ({scrollX}) => {
   return (
     <View style={{ position: 'absolute', bottom: 100, flexDirection:'row'}}>
       {data.map((_, i) => {
+        const inputRange = [ (i- 1) * width, i * width, (i+1) * width ];
+        const scale = scrollX.interpolate({
+          inputRange,
+          outputRange: [0.8, 1.4, 0.8],
+          extrapolate: 'clamp',
+        });
+        const opacity = scrollX.interpolate({
+          inputRange,
+          outputRange: [0.6, 0.9, 0.6],
+          extrapolate: 'clamp',
+        });
         return(
-          <View
+          <Animated.View
             key={`indicator-${i}`}
             style={{
               height: 10,
               width: 10,
               borderRadius: 5,
-              backgroundColor: '#333',
-              margin:10
+              backgroundColor: '#fff',
+              opacity,
+              margin:10,
+              transform: [
+                {
+                  scale,
+                }
+              ]
             }}
           />
         )
@@ -43,6 +65,53 @@ const Backdrop = ({scrollX}) => {
   );
 };
 
+const Square = ({scrollX}) => {
+  const YOLO = Animated.modulo(
+    Animated.divide(
+      Animated.modulo(scrollX, width),
+      new Animated.Value(width)
+  ), 1);
+
+  const rotate = YOLO.interpolate({
+    inputRange: [0, .5, 1],
+    outputRange: [ '35deg', '0deg', '35deg'],
+  })
+
+  const translateX = YOLO.interpolate({
+    inputRange: [0, .5, 1],
+    outputRange: [ 0, - height , 0],
+  })
+
+  return (
+    <Animated.View 
+      style={{
+        width: height,
+        height: height,
+        backgroundColor: '#fff',
+        borderRadius: 86,
+        position: 'absolute',
+        top: -height * 0.7,
+        left: -height * 0.3,
+        transform: [
+          {
+            rotate,
+          },
+          {
+            translateX,
+          }
+        ]
+      }}
+    />
+  )
+}
+
+const PrevItem = () => {
+}
+
+const NextItem = () => {
+  
+}
+
 const Carousel = ({data}) => {
   const scrollX = React.useRef(new Animated.Value(0)).current;
   if(data && data.length){
@@ -50,6 +119,7 @@ const Carousel = ({data}) => {
       <View style={styles.container}>
         <StatusBar hidden />
         <Backdrop scrollX={scrollX}/>
+        <Square scrollX={scrollX}/>
         <Animated.FlatList 
           data={data}
           keyExtractor={ item => item.key}
@@ -64,18 +134,24 @@ const Carousel = ({data}) => {
           pagingEnabled
           renderItem={({item})=>{
             return (
-              <View style={styles.viewData}>
-                <View style={styles.viewImage}>
-                  <Image source ={{uri: item.image} } style={styles.image}/>
-                </View>
-                <View>
-                  <Text style={styles.textTitle}>{item.title}</Text>
-                  <Text style={styles.textDescription}>{item.description}</Text>
-                </View>
-              </View>
+              <CarouselItem item={item}/>
             );
           }}
         />
+        <View style={styles.viewButtom}>
+          <AntDesign
+            name="caretleft"
+            size={30}
+            color="white"
+            onPress={ () => console.log('Prev')  }
+          />
+          <AntDesign
+            name="caretright"
+            size={30}
+            color="white"
+            onPress={ () => console.log('Next')  }
+          />
+        </View>
         <Indicator scrollX={scrollX}/>
       </View>
     );
@@ -84,39 +160,5 @@ const Carousel = ({data}) => {
   console.log('Please provide Images')
   return null
 }
-
-const styles = StyleSheet.create( {
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  viewData: {
-    width, 
-    alignItems: 'center', 
-    padding: 20,
-  },
-  viewImage:{
-    flex: .7,
-    justifyContent: 'center'
-  },
-  image: {
-    width: width / 2,
-    height: height / 2,
-    resizeMode: 'contain'
-  },
-  textTitle: {
-    color: 'white',
-    fontWeight: '800',
-    fontSize: 28,
-    marginBottom: 10,
-  },
-  textDescription: {
-    color: 'white',
-    fontWeight: '300',
-
-  }
-})
 
 export default Carousel;
